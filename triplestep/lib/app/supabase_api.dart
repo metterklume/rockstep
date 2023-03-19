5dd94716a34d035b9c0077fc240acc35990f0f3a
Akanksha Vyas <vyasakanksha@gmail.com>
2023-03-19 13:40:42 +0530

    working prototype

--- triplestep/lib/app/supabase_api.dart
+++ triplestep/lib/app/supabase_api.dart
@@ -4,6 +4,7 @@ import 'package:supabase/supabase.dart';
 const String SUPABASE_URL = 'https://qmwscwbmjyfwlccikrqf.supabase.co';
 
 // use your own SUPABASE_SECRET key
-const String SUPABASE_SECRET = 'ADD_SECRET';
+const String SUPABASE_SECRET =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtd3Njd2Jtanlmd2xjY2lrcnFmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0OTE3MjA5OCwiZXhwIjoxOTY0NzQ4MDk4fQ.gfDpEhLlfxs9-sc89-ZF3sBIlWGVN2G-2W3KKYh3xeA';
 
 final SupabaseClient supabase = SupabaseClient(SUPABASE_URL, SUPABASE_SECRET);
